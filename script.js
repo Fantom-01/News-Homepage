982dbb62b25b8304a596a48be48ba71098bbc7d3
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-02-05 06:52:49 +0100

    final commit(hopefully) menu sidebar done

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let menuIcon = document.getElementById("menu-icon");
 let menu = document.getElementById("menu");
+let menuIconClose = document.getElementById("menu-icon-close");
 
-menuIcon.addEventListener("click", menuDisplay(menu));
+menuIcon.addEventListener("click", menuDisplay);
+menuIconClose.addEventListener("click", menuHide);
 
-function menuDisplay(menu) {
+function menuDisplay() {
   menu.style.display = "block";
+}
+
+function menuHide() {
+  menu.style.display = "none";
 }
